63ce23681b6b1001534904c9287912a01fd5c157
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-17 14:21:37 -0300

    feat: challenge solved

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === 'income' || type === 'outcome') {
+      if (type === 'outcome') {
+        const saldo = this.transactionsRepository.getBalance().total;
+        if (saldo - value <= 0) {
+          throw new Error('Insuficient funds');
+        }
+      }
+      return this.transactionsRepository.create({ title, value, type });
+    }
+    throw new Error('Type of transaction not allowed');
   }
 }
 
